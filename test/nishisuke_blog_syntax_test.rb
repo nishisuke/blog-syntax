af6979856059095fac9fcd660fbf4dbf08b7a497
nishisuke <nishisuke13+git@gmail.com>
2018-01-09 17:13:59 +0900

    Add test

--- test/nishisuke_blog_syntax_test.rb
+++ test/nishisuke_blog_syntax_test.rb
@@ -1,11 +1,13 @@
-require "test_helper"
+require_relative 'test_helper'
 
 class NishisukeBlogSyntaxTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::NishisukeBlogSyntax::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_it_return_html
+    txt = %Q(hogetarou\nSRC```hoge.rb\ndef hoge\n  p 'tarou'\nend\nSRC```\noshimaikeru.)
+    html = NishisukeBlogSyntax.convert_html(txt)
+    assert html
   end
 end
